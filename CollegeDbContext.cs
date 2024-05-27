e55bc3bc55aa7658b844761a756878c26d2b871c
Abhilash1230 <62554344+Abhilash1230@users.noreply.github.com>
2024-05-27 15:08:02 +0530

    Add files via upload

---
+++ CollegeDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIPracticeProject.Models;
+
+namespace WebAPIPracticeProject.Data
+{
+    public class CollegeDbContext:DbContext
+    {
+
+        public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options) {
+        }
+        DbSet<Student> Students {  get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new List<Student>()
+            {
+                new Student{Id=1, Name="DummyData", Address="adrds--", AdmissionDate=DateTime.Now, Age=25}
+            });
+        }
+
+    }
+}
